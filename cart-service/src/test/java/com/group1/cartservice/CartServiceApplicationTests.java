2d943108cc9484730375f1578b913bd1618c7a2e
Paritosh Pundlik Pal <paritoshpal7796@gmail.com>
2026-03-05 22:07:47 +0000

    Cart-Srvice Skeleton

---
+++ cart-service/src/test/java/com/group1/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.group1.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+
+class CartServiceApplicationTests extends AbstractIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ cart-service/src/test/java/com/group1/cartservice/TestCartServiceApplication.java
@@ -0,0 +1,11 @@
+package com.group1.cartservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CartServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
