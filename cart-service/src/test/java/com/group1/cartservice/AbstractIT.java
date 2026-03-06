d3d143a57fcf37b2af015016449d1be5a9997b23
Paritosh Pundlik Pal <paritoshpal7796@gmail.com>
2026-03-06 16:57:27 +0000

    Setting up - Wiremock

--- cart-service/src/test/java/com/group1/cartservice/AbstractIT.java
+++ cart-service/src/test/java/com/group1/cartservice/AbstractIT.java
@@ -1,20 +1,38 @@
 package com.group1.cartservice;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.wiremock.spring.ConfigureWireMock;
+import org.wiremock.spring.EnableWireMock;
+import org.wiremock.spring.InjectWireMock;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableWireMock(@ConfigureWireMock(port = 0))
 public class AbstractIT {
 
     @LocalServerPort
     int port;
 
+    @InjectWireMock
+    protected WireMockServer wireMockServer;
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("cart.catalog-service-url",() -> "http://localhost:${wiremock.server.port}");
+    }
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+        wireMockServer.resetAll();
     }
+
+
 }
