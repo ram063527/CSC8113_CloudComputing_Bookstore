4711b4d8ae6f04b7fccc8f01b1bcc103cda1fba0
Paritosh Pundlik Pal <paritoshpal7796@gmail.com>
2026-03-09 18:11:44 +0000

    Kubernetes manifest files and k6 loadtest script

---
+++ load-test.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '1m', target: 100 }, // Ramp up to 100 users over 1 minute
+        { duration: '3m', target: 500 }, // Ramp up to 500 users and hold for 3 minutes
+        { duration: '1m', target: 0 },   // Ramp down to 0 users over 1 minute
+    ],
+};
+
+export default function () {
+    // Hitting the Catalog Service directly to isolate the CPU load
+    const res = http.get('http://localhost:8081/api/products');
+
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+    });
+
+    // Sleep for 1 second between requests so we don't accidentally DDoS your laptop
+    sleep(1);
+}
