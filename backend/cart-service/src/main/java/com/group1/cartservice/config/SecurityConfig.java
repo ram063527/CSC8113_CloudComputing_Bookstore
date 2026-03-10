5b65ac53f5dde53a5acb6396359064e22f8ef16d
Paritosh Pundlik Pal <paritoshpal7796@gmail.com>
2026-03-10 16:30:49 +0000

    Allowing actuator endpoints

--- backend/cart-service/src/main/java/com/group1/cartservice/config/SecurityConfig.java
+++ backend/cart-service/src/main/java/com/group1/cartservice/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/actuator/health"
+                                "/actuator/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
